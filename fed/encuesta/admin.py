b37d345175072830fa85f708c16d4547541730a1
Helmy Giacoman <helmygb@gmail.com>
2010-09-07 17:24:43 -0600

    updating admin

---
+++ fed/encuesta/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from models import *
+
+admin.site.register(Organizacion)
+admin.site.register(Proyecto)
+admin.site.register(AccionEfectuadaMedio)
+admin.site.register(AccionEfectuadaRegion)
+admin.site.register(AccionEfectuadaDocumento)
+admin.site.register(ParticipacionComisionDecision)
+admin.site.register(ParticipacionComisionAgenda)
+admin.site.register(AccionObservatorio)
