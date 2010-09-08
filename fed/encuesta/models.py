b4b556d847a600630ab5329095563f74867dd99d
Helmy Giacoman <helmygb@gmail.com>
2010-09-08 10:55:03 -0600

    completando models

--- fed/encuesta/models.py
+++ fed/encuesta/models.py
@@ -180,3 +180,98 @@ class DenunciaJuridica(models.Model):
     class Meta:
         verbose_name = 'Denuncia jurídica'
         verbose_name_plural = 'Denuncias jurídicas'
+
+MEDIOS_REFLEXION = (('taller_formacion', 'Talleres de formación'),
+                    ('foros', 'Foros'),
+                    ('intercambio_xp', 'Intercambio de experiencias'),
+                    ('visitas_grupales', 'Visitas grupales'),
+                    ('visitas_indiv', 'Visitas individuales'),
+                    ('teatros', 'Teatros'),
+                    ('circulo_estudio', 'Círculos de estudio'),
+                    ('actos_cultura', 'Actos culturales'),
+                    ('reunion_comunitaria', 'Reuniones comunitarias'), )
+
+class AccionRealizadaReflexion(models.Model):
+    accion = models.CharField(max_length=100, choices=MEDIOS_REFLEXION)
+    mujeres = models.IntegerField('Con las mujeres')
+    hombres = models.IntegerField('Con los hombres')
+    jovenes = models.IntegerField('Con los y las jóvenes')
+    div_sexual = models.IntegerField('Con las personas de la diversidad sexual')
+    vih = models.IntegerField('Con las personas con VIH y SIDA')
+    etnica = models.IntegerField('Con las personas de población étnica e indigena')
+    discapacidad = models.IntegerField('Con las personas con discapacidad')
+
+    def __unicode__(self):
+        return self.accion
+
+    class Meta:
+        verbose_name = 'Acción realizada para reflexión de pob'
+        verbose_name_plural = 'Acciones realizadas para reflexión de pob'
+        
+PERSONAS_REFLEXION = (('personas_participaron', 'Número de personas que participaron en las acciones realizadas para reflexionar sobre derechos sexuales y reproductivos'),
+                      ('personas_participaron_toman_decision', 'Número de personas que participaron y que actualmente pueden tomar decisiones sexuales y reproductivas de manera autonoma y bien informada'), )
+
+class AccionRelizadaReflexionPersona(models.Model):
+    accion = models.CharField(max_length=100, choices=PERSONAS_REFLEXION)
+    mujeres = models.IntegerField('Las mujeres')
+    hombres = models.IntegerField('Los hombres')
+    jovenes = models.IntegerField('Los y las jóvenes')
+    div_sexual = models.IntegerField('Las personas de la diversidad sexual')
+    vih = models.IntegerField('Las personas con VIH y SIDA')
+    etnica = models.IntegerField('Las personas de población étnica e indigena')
+    discapacidad = models.IntegerField('Las personas con discapacidad')
+
+    def __unicode__(self):
+        return self.accion
+
+    class Meta:
+        verbose_name = 'Acción realizada para reflexion Cant'
+        verbose_name_plural = 'Acciones realizadas p/reflexión Cant'
+
+MEDIOS2 = (('tv', 'Campañas por Televisión'),
+           ('radio', 'Campañas radiales'),
+           ('periodico', 'Artículos en periódicos'),
+           ('talleres', 'Talleres de formación'),
+           ('foros', 'Foros'),
+           ('intercambio_xp', 'Intercambio de experiencias'),
+           ('visitas', 'Visitas grupales o individuales'),
+           ('teatros_actos', 'Teatros y actos culturales'),
+           ('circulo_estudio', 'Círculos de estudio'),
+           ('reunion_comu', 'Reuniones comunitarias'),
+           ('material_educativo', 'Materiales educativos'),
+           ('reunion_autorid', 'Reuniones con autoridades'),
+           ('consejeria', 'Consejería y promotoría social'), )
+
+class AccionImpulsadaOrg(models.Model):
+    accion = models.CharField(max_length=100, choices=MEDIOS2)
+    acciones_emprendidas = models.IntegerField('Número de acciones emprendidas por las Org. para la prevención de la violencia basada en género')
+    acciones_cambios_actitud = models.IntegerField('Número de acciones que lograron cambios de actitud de los grupos metas para la prevención de violencia basada en género')
+    acciones_impulsadas_masculinidad = models.IntegerField('Numero y tipo de acciones impulsadas por las Org. para promover masculinidad libre de prejuicios y violencia')
+    acciones_cambios_masculinidad = models.IntegerField('Numero de acciones que lograron cambio de actitud de los grupos metas hacia la masculinidad libre de prejuicios y violencia')
+
+    def __unicode__(self):
+        return self.accion
+
+    class Meta:
+        verbose_name = 'Acción impul. x/Org p/prev violencia '
+        verbose_name_plural = 'Acciones impul. x/Org. p/prev violencia'
+
+class AccionImpulsadaGrupo(models.Model):
+    accion = models.CharField(max_length=100, choices=MEDIOS2)
+    acciones_emprendidas_sex = models.IntegerField('Número y tipo de acciones emprendidas por los grupos de diversidad sexual, para prev de violencia basada en género')
+    acciones_cambio_sex = models.IntegerField('Numero de acciones que lograron cambio de actitud en grupo de diversidad sexual')
+    acciones_emprendidas_discapa = models.IntegerField('Número y tipo de acciones emprendidas por los grupos de personas con discapacidad para prev de violencia basada en género')
+    acciones_cambio_discapa = models.IntegerField('Número de acciones que lograron cambio de actitud en grupo de personas con discapacidad')
+    acciones_emprendidas_etnia = models.IntegerField('Número de acciones emprendidas por los grupos étnicos y pueblos indígenas para la prevención de la violencia basada en género')
+    acciones_cambio_etnia = models.IntegerField('Número de acciones que lograron cambio de actitud en grupos étnicos y pueblos indígenas')
+    acciones_emprendidas_jovenes = models.IntegerField('Número de acciones emprendidas por los y las jóvenes para la prevención de la violencia basada en género')
+    acciones_cambio_jovenes = models.IntegerField('Número de acciones que lograron cambio de actitud en los y las jóvenes ')
+
+    def __unicode__(self):
+        return self.accion
+
+    class Meta:
+        verbose_name = 'Acción impulsada por grupos'
+        verbose_name_plural = 'Acciones impulsadas por grupos'
+
+    
