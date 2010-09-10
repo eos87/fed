17cb15a082c964fce89a853f982e9a2027f4b5d3
Helmy Giacoman <helmygb@gmail.com>
2010-09-10 17:01:31 -0600

    changes

--- fed/lugar/models.py
+++ fed/lugar/models.py
@@ -1,10 +1,12 @@
+# -*- coding: UTF-8 -*-
+
 from django.db import models
 
 class Departamento(models.Model):
-    #id = models.IntegerField("Numero de dept", primary_key=True)
+    id = models.IntegerField("Código", primary_key=True)
     nombre = models.CharField(max_length=30, unique= True)
     slug = models.SlugField(unique=True, null=True, help_text="Usado como url unica(autorellenado)")
-    #extension = models.DecimalField("Extension Territorial", max_digits=10,decimal_places=2)
+    extension = models.DecimalField("Extension Territorial", max_digits=10,decimal_places=2, null=True)
 
     def __unicode__(self):
         return self.nombre
@@ -13,16 +15,16 @@ class Departamento(models.Model):
         verbose_name_plural = "Departamentos"
 
 class Municipio(models.Model):
-    #id = models.IntegerField("Numero de dept", primary_key=True)
+    id = models.IntegerField("Código", primary_key=True)
     departamento = models.ForeignKey(Departamento)
     nombre = models.CharField(max_length=30, unique=True)
     slug = models.SlugField(unique=True, null=True, help_text="Usado como url unica(autorellenado)")
-    #extension = models.DecimalField("Extension Territorial", max_digits=10, decimal_places=2, blank=True, null=True)
+    extension = models.DecimalField("Extension Territorial", max_digits=10, decimal_places=2, blank=True, null=True)
     latitud = models.DecimalField('Latitud', max_digits=8, decimal_places=5, blank=True, null = True)
     longitud = models.DecimalField('Longitud', max_digits=8, decimal_places=5, blank=True, null = True)
 
     def __unicode__(self):
-        return self.nombre
+        return '%s - %s' % (self.departamento, self.nombre)
 
     class Meta:
         verbose_name_plural = "Municipios"
