9b9993a628598ce4b3842ab5a1901ec8e7dd728d
Helmy Giacoman <helmygb@gmail.com>
2010-09-24 17:18:44 -0600

    presentacion primera parte

--- fed/encuesta/views.py
+++ fed/encuesta/views.py
@@ -1,26 +1,23 @@
+from decorators import session_required
 from django.core.exceptions import ViewDoesNotExist
+from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic.simple import direct_to_template
-from decorators import session_required
 from forms import *
 from models import *
 
-def _queryset_filtrado(request):
+def _queryset_filtrado(request, resultado):
     '''metodo para obtener el queryset de encuesta
     segun los filtros del formulario que son pasados
     por la variable de sesion'''
-    params = {}
-    if request.session['organizacion']:
-        params['organizacion__in'] = request.session['organizacion']
-    if request.session['municipio']:
-        proyectos = Proyecto.objects.filter(organizacion__in=request.session['organizacion'], municipio__in=request.session['municipio'])
-        params['proyecto__in'] = proyectos
-    if request.session['desde'] and request.session['hasta']:
-        params['fecha_inicio__range'] = (request.session['desde'], request.session['hasta'])
-
-    return Encuesta.objects.filter(**params)
+    rt = ResultadoTrabajado.objects.filter(resultado=resultado, municipio__in=request.session['municipio'])    
+    list = []
+    for r in rt:
+        if request.session['desde'] <= r.encuesta.fecha_inicio <= request.session['hasta']:
+            list.append(r.encuesta.id)
+    return Encuesta.objects.filter(pk__in=list)
 
 def index(request):
     return direct_to_template(request, 'index.html')
@@ -64,6 +61,7 @@ def indicadores(request):
             request.session['municipio'] = form.cleaned_data['municipio']
             request.session['desde'] = form.cleaned_data['desde']
             request.session['hasta'] = form.cleaned_data['hasta']
+            request.session['activo'] = True
             resultados = Resultado.objects.all()
     else:
         form = IndicadoresForm()
@@ -78,11 +76,78 @@ def resultado1(request):
 #vistas para indicadores, no hay forma de volverlos dinamicos
 @session_required
 def indicador111(request):
-    a = _queryset_filtrado(request)
+    resultado = Resultado.objects.get(pk=1)
+    tabla = {}
+    tabla2 = {}
+    tabla3 = {}
+    a = _queryset_filtrado(request, resultado)
     for opcion in CHOICE_MEDIO:
-        query = a.accionefectuadamedio_set.filter(accion=opcion[0])
+        query = AccionEfectuadaMedio.objects.filter(encuesta__in=a, accion=opcion[0])
+        cantidad_sum = query.aggregate(cantidad_sum=Sum('cantidad'))['cantidad_sum']
+        participantes_sum = query.aggregate(participantes_sum=Sum('participantes'))['participantes_sum']
+        tabla[opcion[1]] = {'cantidad':cantidad_sum, 'participantes': participantes_sum}
+
+
+    for opcion in CHOICE_REGION:
+        query = AccionEfectuadaRegion.objects.filter(encuesta__in=a, accion=opcion[0])
+        cantidad_sum = query.aggregate(cantidad_sum=Sum('cantidad'))['cantidad_sum']
+        participantes_sum = query.aggregate(participantes_sum=Sum('participantes'))['participantes_sum']
+
+        tabla2[opcion[1]] = {'cantidad':cantidad_sum, 'participantes': participantes_sum}
+
+    for opcion in CHOICE_DOCS:
+        query = AccionEfectuadaDocumento.objects.filter(encuesta__in=a, accion=opcion[0])
+        cantidad_sum = query.aggregate(cantidad_sum=Sum('cantidad'))['cantidad_sum']
+        participantes_sum = query.aggregate(participantes_sum=Sum('participantes'))['participantes_sum']
+        prom = get_prom(cantidad_sum, participantes_sum)
+
+        tabla3[opcion[1]] = {'cantidad':cantidad_sum, 'participantes': participantes_sum, 'prom':prom}
 
     return render_to_response('fed/indicador111.html', RequestContext(request, locals()))
+
+@session_required
+def indicador112(request):
+    resultado = Resultado.objects.get(pk=1)
+    tabla = {}
+    a = _queryset_filtrado(request, resultado)
+
+    for opcion in CHOICE_REGION:
+        query = ParticipacionComisionDecision.objects.filter(encuesta__in=a, accion=opcion[0])
+        instancias_sum = query.aggregate(instancias_sum=Sum('cantidad_instancias'))['instancias_sum']
+        acciones_promovidas_sum = query.aggregate(acciones_promovidas_sum=Sum('cantidad_acciones_promovidas'))['acciones_promovidas_sum']
+        acciones_efectivas_sum = query.aggregate(acciones_efectivas_sum=Sum('cantidad_acciones_efectivas'))['acciones_efectivas_sum']
+        prom = get_prom(acciones_promovidas_sum, acciones_efectivas_sum)
+
+        tabla[opcion[1]] = {
+            'instancias':instancias_sum,
+            'acciones_promovidas': acciones_promovidas_sum,
+            'acciones_efectivas': acciones_efectivas_sum,
+            'prom':prom
+            }
+
+    return render_to_response('fed/indicador112.html', RequestContext(request, locals()))
+
+@session_required
+def indicador113(request):
+    resultado = Resultado.objects.get(pk=1)
+    tabla = {}
+    a = _queryset_filtrado(request, resultado)
+
+    for opcion in CHOICE_REGION:
+        query = ParticipacionComisionAgenda.objects.filter(encuesta__in=a, accion=opcion[0])
+        instancias_sum = query.aggregate(instancias_sum=Sum('cantidad_instancias'))['instancias_sum']
+        acciones_promovidas_sum = query.aggregate(acciones_promovidas_sum=Sum('cantidad_acciones_prom'))['acciones_promovidas_sum']
+        acciones_efectivas_sum = query.aggregate(acciones_efectivas_sum=Sum('cantidad_acciones_efec'))['acciones_efectivas_sum']
+        prom = get_prom(acciones_promovidas_sum, acciones_efectivas_sum)
+
+        tabla[opcion[1]] = {
+            'instancias':instancias_sum,
+            'acciones_promovidas': acciones_promovidas_sum,
+            'acciones_efectivas': acciones_efectivas_sum,
+            'prom':prom
+            }
+
+    return render_to_response('fed/indicador113.html', RequestContext(request, locals()))
 
 #obtener la vista adecuada para los indicadores
 def _get_view(request, vista):
@@ -96,4 +161,10 @@ VALID_VIEWS = {
 
     #inicia vistas de indicadores
     'indicador-111': indicador111,
+    'indicador-112': indicador112,
+    'indicador-113': indicador113,
     }
+
+def get_prom(total, cantidad):
+    x = (cantidad * 100) / total
+    return x
