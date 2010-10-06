c1bc1a30549ef6e5b5670eafebd863022e4aa320
Helmy Giacoman <helmygb@gmail.com>
2010-10-06 17:11:35 -0600

    fix imports

--- urls.py
+++ urls.py
@@ -10,7 +10,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 urlpatterns = patterns('',
     (r'^files/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PROJECT_DIR + '/files'}),
     (r'^$','fed.encuesta.views.index'),
-    (r'^', include('encuesta.urls')),
+    (r'^', include('fed.encuesta.urls')),
     
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}),
